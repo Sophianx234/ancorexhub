09bc408fbf6c161397305ee061c86aa9906e069b
sophianx234 <dx4336969@gmail.com>
2026-02-16 21:19:56 +0000

    feat: add ignore eslint error during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    
+  typescript:{
+    ignoreBuildErrors:true
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "60mb", // or "5mb"
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+       {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+    ],
+  },
+
 };
+
+
 
 export default nextConfig;
